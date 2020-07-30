98f406fab397b138cb7b88da10800cf7b9ee43f2
JinyLim <hjdeer@hanmail.net>
2020-07-30 19:04:38 +0900

    5 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import * as wjcCore from 'wijmo/wijmo';
+import * as wjGrid from 'wijmo/wijmo.grid';
+import * as wjcFilter from 'wijmo/wijmo.grid.filter';
+
 
 @Component({
   selector: 'my-app',
@@ -6,5 +10,34 @@ import { Component } from '@angular/core';
   styleUrls: [ './app.component.css' ]
 })
 export class AppComponent  {
-  name = 'Angular';
+  @ViewChild('grid') grid: wjGrid.FlexGrid;
+  gridData: wjcCore.CollectionView = undefined;       
+  _filter : wjcFilter.FlexGridFilter;
+
+
+
+  selectAll: boolean = false;
+    gridSelectAllRows(value: any) {
+        this.selectAll = !value;
+        this.gridData.items.forEach((d:any, ind:number) => {
+            if(d.isSelected !== this.selectAll) {
+                this.grid.setCellData(ind, 'isSelected', this.selectAll);
+            }
+        });
+    }
+    
+    selectedItems : any;//for selecting multiple rows at once
+    gridSelectRow(selectItem: any){
+        if(this.grid.selectedItems.length > 1){            
+            this.selectedItems = this.grid.selectedItems.filter((s)=>{return s !== selectItem});            
+        }else{
+            this.selectedItems = undefined;
+        }
+    }
+
+    gridChecked(value: boolean){
+        if(this.selectedItems){ //multi selection상태이면 선택된 다른 row도 동일하게 Select
+            this.selectedItems.forEach((s)=>{s.isSelected = value;})
+        }
+    }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,24 @@
-import { NgModule } from '@angular/core';
+
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
+import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
+
+import { WjGridModule } from 'wijmo/wijmo.angular2.grid';
+import { WjGridFilterModule } from 'wijmo/wijmo.angular2.grid.filter';
+import { WjGridDetailModule } from 'wijmo/wijmo.angular2.grid.detail';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
+  imports:      [ BrowserModule, 
+      FormsModule ,   
+      WjGridModule,
+        WjGridDetailModule,
+        WjGridFilterModule],
+  declarations: [ AppComponent ],
+  exports: [ WjGridModule,
+        WjGridDetailModule,
+        WjGridFilterModule],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
